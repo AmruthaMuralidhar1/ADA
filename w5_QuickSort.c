5dea5d7affe90d859926575b9d6a07bdb22eb5b7
AmruthaMuralidhar1 <125537105+AmruthaMuralidhar1@users.noreply.github.com>
2023-07-17 14:37:29 +0530

    Update w5_QuickSort.c

--- w5_QuickSort.c
+++ w5_QuickSort.c
@@ -1,38 +1,49 @@
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
+#include<stdlib.h>
+#include<time.h>
+int a[10000];
+void quicksort(int first,int last){
    int i, j, pivot, temp;
    if(first<last){
       pivot=first;
       i=first;
       j=last;
       while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
+         while(a[i]<=a[pivot]&&i<last)
          i++;
-         while(number[j]>number[pivot])
+         while(a[j]>a[pivot])
          j--;
          if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
          }
       }
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
+      temp=a[pivot];
+      a[pivot]=a[j];
+      a[j]=temp;
+      quicksort(first,j-1);
+      quicksort(j+1,last);
    }
 }
 int main(){
-   int i, count, number[25];
-   printf("How many elements are u going to enter?: ");
-   scanf("%d",&count);
-   printf("Enter %d elements: ", count);
-   for(i=0;i<count;i++)
-   scanf("%d",&number[i]);
-   quicksort(number,0,count-1);
-   printf("Order of Sorted elements: ");
-   for(i=0;i<count;i++)
-   printf(" %d",number[i]);
+   int i, n;
+   clock_t start, end;
+   printf("Enter number of elements\n");
+   scanf("%d",&n);
+   for(i=0;i<n;i++){
+   	a[i]=rand()%100;
+   } 
+   printf("Before sorting\n");
+   for(i=0;i<n;i++){
+   	printf("%d\t",a[i]);
+   }
+   start = clock();   
+   quicksort(0,n-1);
+   end = clock(); 
+   printf("\nAfter sorting\n");
+   for(i=0;i<n;i++)
+   	printf("%d\t",a[i]);
+   printf("\nTime = %f\n", ((double)(end-start))/CLOCKS_PER_SEC);
    return 0;
 }
